624ef211dcf89e94c9e98d1438dad9ad3b311a0d
Bram <bgerritsen@emico.nl>
2024-04-20 19:35:23 +0200

    feat: better styling

--- src/components/DetailPanel.tsx
+++ src/components/DetailPanel.tsx
@@ -1,5 +1,6 @@
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 import {PowerProfile} from '../types/PowerProfile'
 import { Alert, CircularProgress, Stack } from '@mui/material';
 import { useQuery } from '@tanstack/react-query'; 
@@ -31,27 +32,26 @@ export const DetailPanel: React.FC<DetailPanelProps> = ({ profile }) => {
   if (isLoading) return <CircularProgress />;
   if (isError) return <Alert severity="error">Error Loading Profile</Alert>;
 
-    return (<Box
-          sx={{
-            display: 'grid',
-            margin: 'auto',
-            gridTemplateColumns: '1fr 1fr',
-            width: '100%',
-          }}
-        >
+    return (
+      <Grid container>
+        <Grid xs={6}>
           <Typography>Manufacturer: {profile.manufacturer}</Typography>
           <Typography>Model ID: {profile.modelId}</Typography>
-          <Typography>DeviceType: {profile.deviceType}</Typography>
+          <Typography>Device type: {profile.deviceType}</Typography>
           <Typography>Name: {profile.name}</Typography>
           <Typography>Created: {fullProfile?.createdAt}</Typography>
           <Typography>Updated: {profile.updatedAt}</Typography>
-          <Typography>Color modes: {profile.colorModes.join(', ')}</Typography>
+        </Grid>
+        <Grid xs={6}>
+          {profile.colorModes.length ? <Typography>Color modes: {profile.colorModes.join(', ')}</Typography> : ''}
           <Typography>Aliases: {profile.aliases}</Typography>
           <Typography>Measure device: {fullProfile?.measureDevice}</Typography>
           <Typography>Measure method: {fullProfile?.measureMethod}</Typography>
           <Typography>Measure description: {fullProfile?.measureDescription}</Typography>
           <Typography>Standby power: {fullProfile?.standbyPower}</Typography>
-        </Box>)
+        </Grid>
+    </Grid>
+    )
 }
 
 const useFetchPowerProfile = (
