fc1a17ead03950c3df9882efd8bec69ea3a080c0
Bram <bgerritsen@emico.nl>
2024-09-17 16:34:40 +0200

    feat: add printer to device types

--- src/types/DeviceType.tsx
+++ src/types/DeviceType.tsx
@@ -2,6 +2,7 @@ export enum DeviceType {
   CAMERA = "camera",
   COVER = "cover",
   LIGHT = "light",
+  PRINTER = "printer",
   NETWORK = "network",
   SMART_SPEAKER = "smart_speaker",
   SMART_SWITCH = "smart_switch",
