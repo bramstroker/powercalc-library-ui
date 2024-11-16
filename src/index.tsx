473a2a6a9e7e50321c146fddaf4413b0ff738955
Bram <bgerritsen@emico.nl>
2024-11-16 19:00:13 +0100

    feat: add manufacturer filter

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,16 @@ import CssBaseline from "@mui/material/CssBaseline";
 import App from "./App";
 import theme from "./theme";
 import { Experimental_CssVarsProvider as CssVarsProvider } from "@mui/material/styles";
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
 root.render(
-  <CssVarsProvider theme={theme} defaultMode="system">
-    <CssBaseline />
-    <App />
-  </CssVarsProvider>,
+  <BrowserRouter>
+    <CssVarsProvider theme={theme} defaultMode="system">
+      <CssBaseline />
+      <App />
+    </CssVarsProvider>
+  </BrowserRouter>
 );
