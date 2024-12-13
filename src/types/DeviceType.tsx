450e9daceefaf5530619ef5b3c983fefc0efe288
Bram <bgerritsen@emico.nl>
2024-12-13 20:04:43 +0100

    feat: add smart dimmer

--- src/types/DeviceType.tsx
+++ src/types/DeviceType.tsx
@@ -4,6 +4,7 @@ export enum DeviceType {
   LIGHT = "light",
   PRINTER = "printer",
   NETWORK = "network",
+  SMART_DIMMER = "smart_dimmer",
   SMART_SPEAKER = "smart_speaker",
   SMART_SWITCH = "smart_switch",
 }
