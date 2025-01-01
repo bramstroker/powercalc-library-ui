295f403d651f88c74fe8f9aca33f652b143fd041
Bram <bgerritsen@emico.nl>
2025-01-01 11:36:28 +0100

    feat: add missing device types

--- src/types/DeviceType.tsx
+++ src/types/DeviceType.tsx
@@ -1,10 +1,13 @@
 export enum DeviceType {
   CAMERA = "camera",
   COVER = "cover",
+  GENERIC_IOT = "generic_iot",
   LIGHT = "light",
+  POWER_METER = "power_meter",
   PRINTER = "printer",
   NETWORK = "network",
   SMART_DIMMER = "smart_dimmer",
   SMART_SPEAKER = "smart_speaker",
   SMART_SWITCH = "smart_switch",
+  VACUUM_ROBOT = "vacuum_robot",
 }
