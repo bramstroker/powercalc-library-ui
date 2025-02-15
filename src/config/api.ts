00b293ddf3cf5d67a03defbb9a489092202fe72f
Bram <bgerritsen@emico.nl>
2025-02-15 19:33:15 +0100

    feat: add more columns to library GUI grid

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,7 +1,7 @@
 export const API_BASE_URL = "https://api.powercalc.nl";
 
 export const API_ENDPOINTS = {
-  LIBRARY: `${API_BASE_URL}/library`,
+  LIBRARY: `${API_BASE_URL}/library?abc`,
   PROFILE: `${API_BASE_URL}/profile`,
   DOWNLOAD: `${API_BASE_URL}/download`,
 };
