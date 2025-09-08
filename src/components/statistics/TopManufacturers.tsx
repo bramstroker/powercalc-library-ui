48901f06a993aabfd74c2a847d9bbbb21255ea23
Bram <bgerritsen@emico.nl>
2025-09-08 10:17:45 +0200

    feat: add query params to filter grid, and add links to statistics

--- src/components/statistics/TopManufacturers.tsx
+++ src/components/statistics/TopManufacturers.tsx
@@ -7,6 +7,7 @@ const TopManufacturers: React.FC = () => {
       title="Top 10 Most Common Manufacturers"
       nameColumnLabel="Manufacturer"
       propertyPath={["manufacturer", "fullName"]}
+      filterQueryParam="manufacturer"
     />
   );
 };
