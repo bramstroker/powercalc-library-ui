7a704fe2d3ae2667c0be0a7fd2219afc4bb73dee
Bram <bgerritsen@emico.nl>
2025-12-29 10:15:16 +0100

    feat: new endpoint

--- src/config/api.ts
+++ src/config/api.ts
@@ -5,6 +5,6 @@ export const API_ENDPOINTS = {
   PROFILE: `${API_BASE_URL}/profile`,
   MANUFACTURER: `${API_BASE_URL}/manufacturer`,
   DOWNLOAD: `${API_BASE_URL}/download`,
-  ANALYTICS_SENSOR_DIMENSIONS: `${API_BASE_URL}/analytics/sensor-dimensions`,
+  ANALYTICS_SENSOR_DIMENSIONS: `${API_BASE_URL}/analytics/sensors`,
   ANALYTICS_SUMMARY: `${API_BASE_URL}/analytics/summary`,
 };
